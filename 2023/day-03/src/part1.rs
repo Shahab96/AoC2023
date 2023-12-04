8e228e8f1f4c62431da130c8f9c3f86306bd5dd8
Shahab Dogar <shahab@dogar.dev>
2023-12-03 19:42:22 -0700

    day 3 part 1 solved

--- 2023/day-03/src/part1.rs
+++ 2023/day-03/src/part1.rs
@@ -1,45 +1,103 @@
 use crate::custom_error::AocError;
 
-#[derive(Debug)]
-struct Coordinates {
-    data: u32,
-    x: usize,
-    y: usize,
-}
+const SEARCH_COORDS: [(i32, i32); 8] = [
+    (0, 1),
+    (1, 0),
+    (0, -1),
+    (-1, 0),
+    (1, 1),
+    (1, -1),
+    (-1, -1),
+    (-1, 1),
+];
 
-fn find_symbol<'a>(
-    line: &str,
-    line_num: usize,
-    prev: Option<&&str>,
-    coords: &'a mut Vec<Coordinates>,
-) -> &'a mut Vec<Coordinates> {
-    let mut x = 0;
+fn check_adjacent(matrix: &Vec<&str>, coords: (usize, usize)) -> bool {
+    for (x, y) in SEARCH_COORDS.iter() {
+        let new_x = coords.0 as i32 + x;
+        let new_y = coords.1 as i32 + y;
 
-    let symbol_idx = line.find(|c: char| c.is_ascii_punctuation());
+        // Check if we are out of the left bound.
+        if new_y < 0 {
+            continue;
+        }
 
-    if symbol_idx.is_none() {
-        return coords;
+        // Check if we are out of the right bound.
+        if new_y >= matrix[coords.0].len() as i32 {
+            continue;
+        }
+
+        // Check if we are out of the top bound.
+        if new_x < 0 {
+            continue;
+        }
+
+        // Check if we are out of the bottom bound.
+        if new_x >= matrix.len() as i32 {
+            continue;
+        }
+
+        // Now we can use usizes
+        let new_x = new_x as usize;
+        let new_y = new_y as usize;
+
+        // Check each of the 8 directions for a non-numeric symbol
+        if let Some(element) = matrix[new_x].chars().nth(new_y) {
+            if element != '.' && !element.is_numeric() {
+                return true;
+            }
+        }
     }
 
-    // TODO:
-    // If you find a symbol, get the numbers in front and behind it.
-    //
-    // Generate coordinates for each number.
-    //
-    // Repeat for the previous line, if the number at the appropriate coordinates is not already in the list.
-
-    todo!()
+    false
 }
 
 #[tracing::instrument]
-pub fn process(input: &str) -> miette::Result<String, AocError> {
-    let mut coords: Vec<Coordinates> = vec![];
-    let lines = input.lines().collect::<Vec<&str>>();
+pub fn process(input: &str) -> miette::Result<u32, AocError> {
+    let matrix = input.lines().collect::<Vec<&str>>();
+    let mut valid_numbers: Vec<u32> = vec![];
+    let mut x = 0;
+    let mut y = 0;
+    let mut line = matrix.get(x).unwrap();
+    let mut chars = line.chars().enumerate();
 
-    for (y, line) in lines.iter().enumerate() {
-        find_symbol(line, y, lines.get(y - 1), &mut coords);
+    loop {
+        if y == matrix[x].len() {
+            x += 1;
+            y = 0;
+
+            if x == matrix.len() {
+                break;
+            }
+
+            line = matrix.get(x).unwrap();
+            chars = line.chars().enumerate();
+            continue;
+        }
+
+        y = chars
+            .find(|(_, c)| c.is_numeric())
+            .map(|(idx, _)| idx)
+            .unwrap_or(matrix[x].len());
+
+        let end_of_num_idx = chars
+            .find(|(_, c)| !c.is_numeric())
+            .map(|(idx, _)| idx)
+            .unwrap_or(matrix[x].len());
+
+        let possible_valid_indices = y..end_of_num_idx;
+
+        for idx in possible_valid_indices {
+            if check_adjacent(&matrix, (x, idx)) {
+                let number = &matrix[x][y..end_of_num_idx];
+                valid_numbers.push(number.parse::<u32>().unwrap());
+                break;
+            }
+        }
+
+        y = end_of_num_idx;
     }
-    todo!()
+
+    Ok(valid_numbers.iter().sum::<u32>())
 }
 
 #[cfg(test)]
