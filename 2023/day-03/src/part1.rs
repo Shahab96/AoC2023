74b32e377b1a6a4ed141e80b8fbd1b57748c2b6d
Shahab Dogar <shahab@dogar.dev>
2023-12-03 19:42:55 -0700

    didn't use test

--- 2023/day-03/src/part1.rs
+++ 2023/day-03/src/part1.rs
@@ -99,16 +99,3 @@ pub fn process(input: &str) -> miette::Result<u32, AocError> {
 
     Ok(valid_numbers.iter().sum::<u32>())
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
-        Ok(())
-    }
-}
