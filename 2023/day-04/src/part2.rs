a79df1ecbfe5f44404ecccc6cc5043f9ab41e773
Shahab Dogar <shahab@dogar.dev>
2023-12-05 09:17:08 -0600

    solve day 4 part 2

--- 2023/day-04/src/part2.rs
+++ 2023/day-04/src/part2.rs
@@ -9,7 +9,7 @@ use nom::{
     IResult,
 };
 
-fn get_num_matches(winning: &[u32], ours: &[u32]) -> u32 {
+fn get_num_matches(winning: &[usize], ours: &[usize]) -> usize {
     let mut matches = 0;
 
     for &number in ours {
@@ -21,16 +21,16 @@ fn get_num_matches(winning: &[u32], ours: &[u32]) -> u32 {
     matches
 }
 
-fn parse_winning_numbers(input: &str) -> IResult<&str, u32> {
+fn parse_winning_numbers(input: &str) -> IResult<&str, usize> {
     let (input, (winning, ours)) = separated_pair(
         separated_list1(
             space1,
-            map_res(preceded(space0, digit1), |s: &str| s.parse::<u32>()),
+            map_res(preceded(space0, digit1), |s: &str| s.parse::<usize>()),
         ),
         tag(" | "),
         separated_list1(
             space1,
-            map_res(preceded(space0, digit1), |s: &str| s.parse::<u32>()),
+            map_res(preceded(space0, digit1), |s: &str| s.parse::<usize>()),
         ),
     )(input)?;
 
@@ -39,65 +39,35 @@ fn parse_winning_numbers(input: &str) -> IResult<&str, u32> {
     Ok((input, matches))
 }
 
-fn parse_card_number(line: &str) -> IResult<&str, u32> {
+fn parse_card_number(line: &str) -> IResult<&str, usize> {
     let (input, card_number) = delimited(
         tag("Card"),
-        map_res(preceded(space1, digit1), |d: &str| d.parse::<u32>()),
+        map_res(preceded(space1, digit1), |d: &str| d.parse::<usize>()),
         terminated(tag(":"), space1),
     )(line)?;
 
     Ok((input, card_number))
 }
 
-fn parse_line(line: &str) -> IResult<&str, (u32, u32)> {
+fn parse_line(line: &str) -> IResult<&str, (usize, usize)> {
     let (input, card_number) = parse_card_number(line)?;
     let (_, matches) = parse_winning_numbers(input)?;
 
     Ok(("", (card_number, matches)))
 }
 
-// fn calculate_next_match_set(card_number: u32, num_matches: u32, cards: &HashMap<u32, u32>) -> u32 {
-//     if num_matches == 0 {
-//         return 0;
-//     }
-//
-//     let mut total = num_matches;
-//     let range = (card_number + 1)..=(card_number + num_matches);
-//
-//     for i in range {
-//         let next_card = cards.get(&i).unwrap();
-//         total += calculate_next_match_set(i, *next_card, cards);
-//     }
-//
-//     total
-// }
-
 #[tracing::instrument]
-pub fn process(input: &str) -> miette::Result<u32> {
-    let cards = input
-        .lines()
-        .map(|line| parse_line(line).unwrap().1 .1)
-        .collect::<Vec<_>>();
+pub fn process(input: &str) -> miette::Result<usize> {
+    let mut counts = vec![1; input.lines().count()];
 
-    let tree = (0..cards.len())
-        .map(|idx| (idx, 1))
-        .collect::<BTreeMap<usize, u32>>();
+    input.lines().for_each(|line| {
+        let (card_number, matches) = parse_line(line).unwrap().1;
+        for card in card_number..(card_number + matches) {
+            counts[card] += counts[card_number - 1];
+        }
+    });
 
-    let total = cards
-        .iter()
-        .enumerate()
-        .fold(tree, |mut acc, (idx, num_matches)| {
-            for i in (idx + 1)..(idx + 1 + *num_matches as usize) {
-                let copies = *acc.get(&i).unwrap();
-                acc.entry(i).and_modify(|e| *e += copies);
-            }
-
-            acc
-        })
-        .values()
-        .sum::<u32>();
-
-    Ok(total)
+    Ok(counts.iter().sum())
 }
 
 #[cfg(test)]
@@ -113,7 +83,7 @@ mod tests {
     #[case("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", (4, 1))]
     #[case("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", (5, 0))]
     #[case("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", (6, 0))]
-    fn test_process(#[case] line: &str, #[case] expected: (u32, u32)) -> miette::Result<()> {
+    fn test_process(#[case] line: &str, #[case] expected: (usize, usize)) -> miette::Result<()> {
         assert_eq!(expected, parse_line(line).unwrap().1);
         Ok(())
     }
