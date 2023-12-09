6b34fb60d666a56fd3dc19a3387cfb45564ca8f7
Shahab Dogar <shahab@dogar.dev>
2023-12-08 21:23:17 -0600

    don't try to brute force day 5

--- 2023/day-05/src/part1.rs
+++ 2023/day-05/src/part1.rs
@@ -1,6 +1,7 @@
-use std::{collections::HashMap, ops::Range};
+use std::collections::HashMap;
 
 use crate::custom_error::AocError;
+use itertools::Itertools;
 use nom::{
     bytes::complete::{tag, take_until},
     character::complete::{anychar, digit1, line_ending, space1},
@@ -10,17 +11,29 @@ use nom::{
     IResult,
 };
 
-fn parse_range_line<'a>(input: &'a str) -> IResult<&'a str, (Range<usize>, Range<usize>)> {
-    let (input, (dest, source, range)) = tuple((
+fn parse_range_line<'a>(input: &'a str) -> IResult<&'a str, HashMap<u32, u32>> {
+    let (input, (destination, source, range)) = tuple((
         map_res(terminated(digit1, space1), |s: &str| s.parse::<usize>()),
         map_res(terminated(digit1, space1), |s: &str| s.parse::<usize>()),
         map_res(digit1, |s: &str| s.parse::<usize>()),
     ))(input)?;
 
-    Ok((input, (dest..range, source..range)))
+    let destination = destination..range;
+    let source = source..range;
+
+    let destination = destination.collect_vec();
+    let source = source.collect_vec();
+
+    let map = source
+        .iter()
+        .zip(destination.iter())
+        .map(|(src, dst)| (*src as u32, *dst as u32))
+        .collect::<HashMap<u32, u32>>();
+
+    Ok((input, map))
 }
 
-fn parse_mapping(input: &str) -> IResult<&str, Vec<(Range<usize>, Range<usize>)>> {
+fn parse_mapping(input: &str) -> IResult<&str, Vec<HashMap<u32, u32>>> {
     separated_list1(line_ending, parse_range_line)(input)
 }
 
@@ -45,50 +58,64 @@ pub fn process(input: &str) -> miette::Result<String, AocError> {
     let (input, seeds) = parse_seeds(input).unwrap();
     let (input, _) = parse_heading(input).unwrap();
     let (input, mapping) = parse_mapping(input).unwrap();
-    let seed_to_soil_map = mapping
-        .into_iter()
-        .map(|(dst, src)| (src, dst))
-        .collect::<HashMap<_, _>>();
+    // let seed_to_soil_map: HashMap<u32, u32> = mapping
+    //     .into_iter()
+    //     .flat_map(|map| map.into_iter())
+    //     .collect();
     let (input, _) = parse_heading(input).unwrap();
     let (input, mapping) = parse_mapping(input).unwrap();
-    let soil_to_fertilizer_map = mapping
-        .into_iter()
-        .map(|(dst, src)| (src, dst))
-        .collect::<HashMap<_, _>>();
+    // let soil_to_fertilizer_map: HashMap<u32, u32> = mapping
+    //     .into_iter()
+    //     .flat_map(|map| map.into_iter())
+    //     .collect();
     let (input, _) = parse_heading(input).unwrap();
     let (input, mapping) = parse_mapping(input).unwrap();
-    let fertilizer_to_water_map = mapping
-        .into_iter()
-        .map(|(dst, src)| (src, dst))
-        .collect::<HashMap<_, _>>();
+    // let fertilizer_to_water_map: HashMap<u32, u32> = mapping
+    //     .into_iter()
+    //     .flat_map(|map| map.into_iter())
+    //     .collect();
     let (input, _) = parse_heading(input).unwrap();
     let (input, mapping) = parse_mapping(input).unwrap();
-    let water_to_light_map = mapping
-        .into_iter()
-        .map(|(dst, src)| (src, dst))
-        .collect::<HashMap<_, _>>();
+    // let water_to_light_map: HashMap<u32, u32> = mapping
+    //     .into_iter()
+    //     .flat_map(|map| map.into_iter())
+    //     .collect();
     let (input, _) = parse_heading(input).unwrap();
     let (input, mapping) = parse_mapping(input).unwrap();
-    let light_to_temperature_map = mapping
-        .into_iter()
-        .map(|(dst, src)| (src, dst))
-        .collect::<HashMap<_, _>>();
+    // let light_to_temperature_map: HashMap<u32, u32> = mapping
+    //     .into_iter()
+    //     .flat_map(|map| map.into_iter())
+    //     .collect();
     let (input, _) = parse_heading(input).unwrap();
     let (input, mapping) = parse_mapping(input).unwrap();
-    let temperature_to_humidity_map = mapping
+    let temperature_to_humidity_map: HashMap<u32, u32> = mapping
         .into_iter()
-        .map(|(dst, src)| (src, dst))
-        .collect::<HashMap<_, _>>();
-    let (input, _) = parse_heading(input).unwrap();
-    let (_, mapping) = parse_mapping(input).unwrap();
-    let humidity_to_wind_map = mapping
-        .into_iter()
-        .map(|(dst, src)| (src, dst))
-        .collect::<HashMap<_, _>>();
+        .flat_map(|map| map.into_iter())
+        .collect();
+    // let (input, _) = parse_heading(input).unwrap();
+    // let (_, mapping) = parse_mapping(input).unwrap();
+    // let humidity_to_location_map: HashMap<u32, u32> = mapping
+    //     .into_iter()
+    //     .flat_map(|map| map.into_iter())
+    //     .collect();
 
-    let mut seed_to_location_map = HashMap::<usize, usize>::new();
-
-    for seed in seeds {}
+    // let mut seed_to_location_map = HashMap::<usize, usize>::new();
+    //
+    // seeds.iter().for_each(|seed| {
+    //     let soil = seed_to_soil_map.get(seed).unwrap();
+    //     let fertilizer = soil_to_fertilizer_map.get(soil).unwrap();
+    //     let water = fertilizer_to_water_map.get(fertilizer).unwrap();
+    //     let light = water_to_light_map.get(water).unwrap();
+    //     let temperature = light_to_temperature_map.get(light).unwrap();
+    //     let humidity = temperature_to_humidity_map.get(temperature).unwrap();
+    //     let location = humidity_to_location_map.get(humidity).unwrap();
+    //
+    //     // seed_to_location_map.insert(*seed, *location);
+    //     println!(
+    //         "seed: {}, location: {}, humidity: {}, temperature: {}, light: {}, water: {}, fertilizer: {}, soil: {}",
+    //         seed, location, humidity, temperature, light, water, fertilizer, soil
+    //     );
+    // });
 
     todo!()
 }
